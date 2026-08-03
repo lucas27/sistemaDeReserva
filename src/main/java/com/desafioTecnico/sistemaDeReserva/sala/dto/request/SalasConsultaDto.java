df4831e3be085d462be1540afdaa3d8dc63e26e2
lucas27 <lucasbrasileiro27@gmail.com>
2026-08-02 21:03:32 -0300

    remove: dtos desnecessarios

--- src/main/java/com/desafioTecnico/sistemaDeReserva/sala/dto/request/SalasConsultaDto.java
+++
@@ -1,9 +0,0 @@
-package com.desafioTecnico.sistemaDeReserva.sala.dto.request;
-
-import io.swagger.v3.oas.annotations.media.Schema;
-
-@Schema(name = "Requisição de consulta de salas", description = "Payload para consular salas livres")
-public record SalasConsultaDto(
-    String nome,
-    String endereco
-) {}
